7a440593419e8fa4527318b95d69d102dc2415a9
Theodore Tio <theodoretkk@gmail.com>
2026-08-15 11:56:47 +0800

    green: has() finds an added item and rejects a missing one

--- ex00/Inventory.cpp
+++ ex00/Inventory.cpp
@@ -1,12 +1,13 @@
 #include "Inventory.hpp"
 
+#include <algorithm>
+
 void Inventory::add(const std::string &item) { m_items.push_back(item); }
 
 void Inventory::remove(const std::string &item) { (void)item; }
 
 bool Inventory::has(const std::string &item) const {
-  (void)item;
-  return true;
+  return std::find(m_items.begin(), m_items.end(), item) != m_items.end();
 }
 
 int Inventory::count() const { return m_items.size(); }
